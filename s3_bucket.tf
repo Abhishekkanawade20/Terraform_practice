960123f5509ec04333eacebbeaaf587ad6792862
Abhishek <abhishekkanawade2000@gmail.com>
2024-03-09 19:27:26 +0530

    --- Auto Git Commit ---

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,15 +1,15 @@
- provider "aws" {
-     region = "ap-south-1"
- }
+#  provider "aws" {
+#      region = "ap-south-1"
+#  }
 
- resource "aws_s3_bucket" "my_terraform_bucket" {
+#  resource "aws_s3_bucket" "my_terraform_bucket" {
 
-     bucket = "terraformbucket9130"
+#      bucket = "terraformbucket9130"
 
-     tags = {
+#      tags = {
 
-         Name = "my terraform bucket"
-         Environment = "staging"
-     }
+#          Name = "my terraform bucket"
+#          Environment = "staging"
+#      }
   
- }
+#  }
