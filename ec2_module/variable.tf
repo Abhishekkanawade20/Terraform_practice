7c2cf8fa62f70c2b39e9cc79ffaef10de8522fe2
Abhishek <abhishekkanawade2000@gmail.com>
2024-03-10 14:25:28 +0530

    --- Auto Git Commit ---

--- ec2_module/variable.tf
+++ ec2_module/variable.tf
@@ -1,27 +1,27 @@
-# # In variable file we defined the variable name that we have created in main.tf type of variable and description
+# In variable file we defined the variable name that we have created in main.tf type of variable and description
 
-# variable "region" {                                # defined variable type and description here
-#     type = string
-#     description = "defined the type region"  
-# }
+variable "region" {                                # defined variable type and description here
+    type = string
+    description = "defined the type region"  
+}
 
-# variable "instance_type" {
-#     type = string
-#     description = "Defined the type of instance type"  
-# }
+variable "instance_type" {
+    type = string
+    description = "Defined the type of instance type"  
+}
 
-# variable "key_name" {
-#     type = string
-#     description = "Defined the key name type"
-# }
+variable "key_name" {
+    type = string
+    description = "Defined the key name type"
+}
 
-# variable "tags" {
-#     type = map
-#     description = "Defined the type of tags"  
-# }
+variable "tags" {
+    type = map
+    description = "Defined the type of tags"  
+}
 
-# variable "ami" {
-#     type = number
-#     description = "Defined ami type"
+variable "ami" {
+    type = number
+    description = "Defined ami type"
   
-# }
+}
