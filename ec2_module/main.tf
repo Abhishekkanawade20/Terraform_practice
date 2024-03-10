161cf40c591c1e90c919c1edafce450d43649cbe
Abhishek <abhishekkanawade2000@gmail.com>
2024-03-10 19:48:38 +0530

    --- Auto Git Commit ---

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -4,18 +4,8 @@ provider "aws" {
   region = var.region
   
 }
-terraform {
-  backend "s3" {
-    bucket = "terraformbucket-21"
-    region = "ap-south-1"
-    key = "terraform.tfstate"
-    dynamodb_table = "terraform_table"
-  }
-}
 
 resource "aws_instance" "abhishek" {
     ami = var.ami
     instance_type = var.instance_type
-    key_name = var.key_name
-    tags = var.tags 
 }
