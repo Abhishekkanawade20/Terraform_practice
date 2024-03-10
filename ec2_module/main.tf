fde823e7853228b57af0cb2cbec3c3b41055b6e2
Abhishek <abhishekkanawade2000@gmail.com>
2024-03-10 12:58:31 +0530

    --- Auto Git Commit ---

---
+++ ec2_module/main.tf
@@ -0,0 +1,20 @@
+#Defining provider, terraform backed, state locking and terraform resource in main file. 
+provider "aws" {
+    region = var.region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraformbucket-21"
+    region = "ap-south-1"
+    key = "terraform.tfstate"
+    dynamodb_table = "terraform_table"
+  }
+}
+
+resource "aws_instance" "abhishek" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    tags = var.tags 
+}
